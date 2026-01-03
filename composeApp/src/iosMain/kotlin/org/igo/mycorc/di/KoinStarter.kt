42cd611507453f5620f8fa21b133d8ea89b4e164
koassumo <koassumo@gmail.com>
2026-01-03 14:32:49 +0100

    koin ios

---
+++ composeApp/src/iosMain/kotlin/org/igo/mycorc/di/KoinStarter.kt
@@ -0,0 +1,9 @@
+package org.igo.mycorc.di
+
+import org.koin.core.context.startKoin
+
+fun startKoinIos() {
+    // Вызываем нашу общую функцию initKoin из commonMain
+    // Здесь можно передать ios-специфичные настройки, если понадобятся
+    initKoin()
+}
--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
+import ComposeApp // 👈 Импортируем наш общий модуль (имя из build.gradle)
 
 @main
 struct iOSApp: App {
+
+    // 👇 Добавляем блок инициализации Koin
+    init() {
+        KoinStarterKt.startKoinIos()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
