cdadcde693720476b8e238fad380550b52977163
koassumo <koassumo@gmail.com>
2026-02-07 17:14:16 +0100

    del

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,8 +69,6 @@ kotlin {
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodelCompose)
-            implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.datetime)
             //
             implementation(compose.materialIconsExtended)
